1305ef82445f5c55c7ea2896c271b15242b4c45b
Dawit-coder <dawit3945@gmail.com>
2024-10-23 19:00:50 +0300

    update build script for netlify deployment

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:5001/clone-c587f/us-central1/api'
+    // baseURL: 'http://127.0.0.1:5001/clone-c587f/us-central1/api'
+
+    //deployed version of amazon on render.com
+    baseURL: 'https://amazon-api-twan.onrender.com'
 })
 
 export {axiosInstance}
